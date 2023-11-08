c0e92f45ea945058655f52430a568b3b51fa07e0
Cota1860 <kotaaaa1122@gmail.com>
2023-11-08 22:12:32 +0900

    Project starts.

---
+++ src/main/java/blanco/BlancoApplication.java
@@ -0,0 +1,13 @@
+package blanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/blanco/BlancoApplicationTests.java
@@ -0,0 +1,13 @@
+package blanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
