b1df786cab03880e73baac2106a81ab23b9321cc
Cota1860 <kotaaaa1122@gmail.com>
2024-03-22 23:13:02 +0900

    java

--- app/src/main/java/blanco/controller/api/ApiController.java
+++ app/src/main/java/blanco/controller/api/ApiController.java
@@ -1,10 +1,16 @@
 package blanco.controller.api;
 
+import java.util.Objects;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import blanco.controller.api.dto.MessageOut;
+import blanco.controller.api.dto.MessagePostIn;
+import blanco.controller.api.dto.MessagePostOut;
 
 @RestController
 @RequestMapping("api")
@@ -14,6 +20,18 @@ public class ApiController {
 	 public MessageOut test() {
 		 MessageOut out = new MessageOut();
 		 out.setContent("How are you?");
+		 return out;
+	 }
+
+	 @PostMapping("/post")
+	 public MessagePostOut testPost(@RequestBody MessagePostIn in) {
+		MessagePostOut out = new MessagePostOut();
+		 if (Objects.equals(in.getTitle(), "sample")){
+			out.setResult("exito");
+		 }else {
+			out.setResult("falla");
+		 }
+
 		 return out;
 	 }
 }
---
+++ app/src/main/java/blanco/controller/api/dto/MessagePostOut.java
@@ -0,0 +1,14 @@
+package blanco.controller.api.dto;
+
+public class MessagePostOut {
+	private String result;
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+	
+}
